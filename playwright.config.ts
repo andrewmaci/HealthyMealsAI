d8f2070ca9675f10de87684a9ea0c82aab2c4d9d
andrewmaci <andrzej.maciaszek009@gmail.com>
2025-11-11 11:59:21 +0100

    fix: github action fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
 
   // Run your local dev server before starting the tests
   webServer: {
-    command: "npm run dev",
+    command: process.env.CI ? "npm run dev:e2e" : "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
